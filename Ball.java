0218d062871281d7e7e2ad2b7d0a781a72a12793
RaskieL <107373121+RaskieL@users.noreply.github.com>
2023-10-14 13:16:53 +0200

    Add ball collisions (buggy)

--- Ball.java
+++ Ball.java
@@ -4,10 +4,10 @@ public class Ball {
     static final int XSCALE = 1;
     static final int YSCALE = 1;
 
-    static final double GRAVITYPULL = 9.81;
+    static final double GRAVITYPULL = 0.0004905;
     static final double FRICTION = 0.0000004;
-    static final double IMPACTINERTIALOSS = 0.001;
     static final double BALLDENSITY = 0.00221043324;
+    static final double IMPACTINERTIALOSS = 0.001;
 
     double ballY;
     double ballX;
@@ -16,8 +16,10 @@ public class Ball {
     double initialRandom;
     int color1; int color2; int color3;
     double ballYvel; double ballXvel;
-    double ballSize; double ballMass; double ballEnergy;
+    double ballSize; double ballMass; double ballEnergyX; double ballEnergyY;
     double ballGravityPull;
+
+    double distanceXcollision; double distanceYcollision; double distancecollision;
 
     public Ball(double speed){
         minSpeed = -0.01;
@@ -26,9 +28,11 @@ public class Ball {
         ballYvel = speed;
 
         ballSize = 0.08;
+
         ballMass = (BALLDENSITY * (4/3*Math.PI*Math.pow(ballSize/2, 3)))/1000; // En kilogramme | Densité en g/m3 | taille arbitrairement en m.
-        ballEnergy = 0.5* (ballMass)*Math.pow(ballXvel*100+ballYvel*100,2); // En Joule | Vitesse en m/s (0.01*100)
-        ballGravityPull = (ballMass)*GRAVITYPULL; // En Newton
+        ballEnergyX = 1/2*(ballMass)*Math.pow((ballXvel*100),2); // En Joule | Vitesse en m/s (0.01*100)
+        ballEnergyY = 1/2*(ballMass)*Math.pow((ballYvel*100),2); // En Joule | Vitesse en m/s (0.01*100)
+        ballGravityPull = ballMass*GRAVITYPULL; // En Newton
 
         color1 = ThreadLocalRandom.current().nextInt(0, 255);
         color2 = ThreadLocalRandom.current().nextInt(0, 255);
@@ -39,39 +43,79 @@ public class Ball {
 
     public void checkCollision(){
         if(ballY - ballSize < -YSCALE){
-                ballYvel += IMPACTINERTIALOSS+ballYvel/1000;
+                ballYvel += IMPACTINERTIALOSS;
                 ballY = -YSCALE + ballSize;
                 ballYvel = -ballYvel;
             }else if(ballY + ballSize > YSCALE){
-                ballYvel -= IMPACTINERTIALOSS+ballYvel/1000;
+                ballYvel -= IMPACTINERTIALOSS;
                 ballY = YSCALE - ballSize;
                 ballYvel = -ballYvel;
             }
         if(ballX - ballSize < -XSCALE){
-                ballXvel += IMPACTINERTIALOSS+ballXvel/1000;
+                ballXvel += IMPACTINERTIALOSS;
                 ballX = -XSCALE + ballSize;
                 ballXvel = -ballXvel;
             }else if(ballX + ballSize > XSCALE){
-                ballXvel -= IMPACTINERTIALOSS+ballXvel/1000;
+                ballXvel -= IMPACTINERTIALOSS;
                 ballX = XSCALE - ballSize;
                 ballXvel = -ballXvel;
             }
     }
 
+    public void checkCollisionBall(Ball ball1, Ball ball2){
+        distanceXcollision = ball1.ballX - ball2.ballX;
+        distanceYcollision = ball1.ballY - ball2.ballY;
+        distancecollision = Math.abs(distanceXcollision+distanceYcollision);
+
+        if(distancecollision <= ballSize){
+            if(distanceYcollision <= ball1.ballY && distanceYcollision >= ball2.ballY){
+                ball1.ballYvel += ball2.ballYvel*ball1.ballYvel - IMPACTINERTIALOSS;
+                //ball1.ballY = ball2.ballY + ball2.ballSize;
+                ball1.ballYvel = -ball1.ballYvel;
+
+                ball2.ballYvel -= ball2.ballYvel*ball1.ballYvel - IMPACTINERTIALOSS;
+                ball2.ballYvel = -ball2.ballYvel;
+
+            }else if(distanceYcollision >= ball1.ballY && distanceYcollision <= ball2.ballY){
+                ball1.ballYvel -= ball2.ballYvel*ball1.ballYvel - IMPACTINERTIALOSS;
+                //ball1.ballY = ball2.ballY - ball2.ballSize;
+                ball1.ballYvel = -ball1.ballYvel;
+
+                ball2.ballYvel += ball2.ballYvel*ballYvel - IMPACTINERTIALOSS;
+                ball2.ballYvel = -ball2.ballYvel;
+            }
+            if(distanceXcollision <= ball1.ballX && distanceXcollision >= ball2.ballX){
+                ball1.ballXvel += ball2.ballXvel*ball1.ballXvel - IMPACTINERTIALOSS;
+                //ball1.ballX = ball2.ballX + ball2.ballSize;
+                ball1.ballXvel = -ball1.ballXvel;
+
+                ball2.ballXvel -= ball2.ballXvel*ball1.ballXvel - IMPACTINERTIALOSS;
+                ball2.ballXvel = -ball2.ballXvel;
+
+            }else if(distanceXcollision >= ball1.ballX && distanceXcollision <= ball2.ballX){
+                ball1.ballXvel -= ball2.ballXvel*ball1.ballXvel - IMPACTINERTIALOSS;
+                //ball1.ballX = ball2.ballX - ball2.ballSize;
+                ball1.ballXvel = -ball1.ballXvel;
+
+                ball2.ballXvel += ball2.ballXvel*ball1.ballXvel - IMPACTINERTIALOSS;
+                ball2.ballXvel = -ball2.ballXvel;
+            }
+
+        }
+    }
+
     public void BallUpdate(){
-        StdDraw.setPenColor(StdDraw.WHITE);
-        StdDraw.setPenRadius(ballSize+(ballSize*0.2));
-        StdDraw.point(ballX, ballY);
         StdDraw.setPenColor(this.color1, this.color2, this.color3);
         StdDraw.setPenRadius(ballSize);
+        if(ballY - ballSize > -YSCALE) ballYvel -= GRAVITYPULL;
         if(ballY - ballSize == -YSCALE && ballXvel > 0) ballXvel -= FRICTION;
         if(ballY - ballSize == -YSCALE && ballXvel < 0) ballXvel += FRICTION;
         ballX += ballXvel; 
-        ballY += ballYvel-ballGravityPull;
+        ballY += ballYvel;
         StdDraw.point(ballX, ballY);
     }
 
     public void jumpBall(){
-        ballYvel += ThreadLocalRandom.current().nextDouble(0.0, 0.0005);
+        ballYvel += ThreadLocalRandom.current().nextDouble(0.0, maxSpeed/2);
     }
 }
--- BallRun.java
+++ BallRun.java
@@ -5,17 +5,24 @@ public class BallRun {
         StdDraw.enableDoubleBuffering();
         final double minspeed = -0.015;
         final double maxspeed = 0.015;
+        final int BALLNUMBER = 10;
 
         StdDraw.setXscale(-Ball.XSCALE,Ball.XSCALE);
         StdDraw.setYscale(-Ball.YSCALE,Ball.YSCALE);
-        Ball[] balls = new Ball[50];
+        Ball[] balls = new Ball[BALLNUMBER];
         for(int i = 0; i < balls.length; i++){
             balls[i] = new Ball(ThreadLocalRandom.current().nextDouble(minspeed, maxspeed));
         }
         while(true){
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.filledRectangle(0, 0, 1, 1);
             for(Ball ball : balls){
-                ball.BallUpdate();
                 ball.checkCollision();
+                /*for(Ball ball2 : balls){
+                    ball.checkCollisionBall(ball, ball2);
+                    ball.checkCollisionBall(ball2, ball);
+                }*/
+                ball.BallUpdate();
             }
             if(StdDraw.isMousePressed()){
                 for(Ball ball : balls){
